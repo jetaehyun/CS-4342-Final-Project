d6ea28624ea31e47ad5a9d56a26f59be68d59357
jetaehyun <jetaehyun0@gmail.com>
2021-05-01 12:11:24 -0400

    random forest

--- random_forest.py
+++ random_forest.py
@@ -3,13 +3,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
+def tune_random_forest():
+    # sample size
+    # sklearn hyper parameter tuning...
+    pass
+
+
 def run_random_forest(train_d, test_d, n_estimators=100):
     Xtr = getData(train_d)
     ytr = getLabels(train_d)
     Xte = getData(test_d)
     yte = getLabels(test_d)
 
-    Xtr, Xte_ex, ytr, yte_ex = train_test_split(Xtr, ytr, test_size=0.2, random_state=20)
+    Xtr, Xte_ex, ytr, yte_ex = train_test_split(Xtr, ytr, test_size=0.5, random_state=42)
 
 
     # n_estimators is number of trees
